cbb18b03fa1863612429f737e67c474c9b4eaa18
Preetam Joshi <Preetam Joshi>
2015-03-15 20:11:44 -0700

    Adding git ignore and substring matcher file.

--- algos/src/main/java/com/algo/simple/SubStringChecker.java
+++ algos/src/main/java/com/algo/simple/SubStringChecker.java
@@ -1,7 +1,40 @@
 package com.algo.simple;
 
-/**
- * Created by preetamjoshi on 3/15/15.
- */
+
 public class SubStringChecker {
+
+
+    boolean isSubstring(String target, String inp){
+        if(isNullOrEmpty(target) || isNullOrEmpty(inp)){
+            return false;
+        }
+
+        for(int i = 0; i < inp.length(); i++){
+            boolean matchFound = true;
+            for(int j = 0; j < target.length(); j++){
+                if(inp.charAt(i+j) != target.charAt(j)) {
+                    matchFound = false;
+                    break;
+                }
+            }
+            if(matchFound){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isNullOrEmpty(String s){
+        if(s == null || s.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String... args){
+        SubStringChecker checker = new SubStringChecker();
+
+        System.out.println(checker.isSubstring("bat", "abate"));
+        System.out.println(checker.isSubstring("bat", "beat"));
+    }
 }
